b84ffda143f42671159996e2c46e5f30e3c4c55b
Ashish Singh <ashish.k.singh1001@gmail.com>
2021-09-11 23:54:49 +0530

    bloc and ui

---
+++ lib/posts/view/posts_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
+
+import '../../posts/post.dart';
+
+class PostsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Posts')),
+      body: BlocProvider(
+        create: (_) => PostBloc(httpClient: http.Client())..add(PostFetched()),
+        child: PostsList(),
+      ),
+    );
+  }
+}
